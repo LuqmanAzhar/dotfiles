d280d60ea759660379dc64f1c6947a6e514e28d9
Luqman Azhar <Luqman_Azhar@hotmail.com>
2023-07-01 04:40:26 +0800

    Added Alternative Commenting key

--- nvim/after/plugin/comment-nvim.lua
+++ nvim/after/plugin/comment-nvim.lua
@@ -9,3 +9,11 @@ vim.keymap.set('v', '<C-/>', function()
             vim.api.nvim_feedkeys(esc, 'nx', false)
             api.toggle.linewise(vim.fn.visualmode())
         end)
+
+-- Also Has alternate k for comment
+vim.keymap.set("n", "<C-k>", api.toggle.linewise.current)
+vim.keymap.set("i", "<C-k>", api.toggle.linewise.current)
+vim.keymap.set('v', '<C-k>', function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            api.toggle.linewise(vim.fn.visualmode())
+        end)
