6827398e29b2eef758a8e368602c277e83115cc4
Luqman Azhar <Luqman_Azhar@hotmail.com>
2023-07-11 23:03:28 +0800

    vim simple removing bindings which already exist

--- nvim/lua/luqmanazhar/remap.lua
+++ nvim/lua/luqmanazhar/remap.lua
@@ -1,9 +1,6 @@
 -- Remapping "project view" 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- Makes Deleting Similar to other editors
-vim.keymap.set("i", "<C-BS>", "<C-W>")
-
 -- Vscode Moving Lines but Vim like
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
