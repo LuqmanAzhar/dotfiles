b487f75dd8ff07989c15739462360cf4ba4c6466
Luqman Azhar <ContactMe@LuqmanAzhar.com>
2023-08-10 09:17:11 +0800

    adding sourcekit swift macOS only

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -15,7 +15,7 @@ lsp.on_attach(function(_, bufnr)
     local opts = {buffer = bufnr, remap = false }
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() print(bufnr) end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
@@ -30,6 +30,9 @@ lsp.on_attach(function(_, bufnr)
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').sourcekit.setup {
+    cmd = {"/usr/bin/sourcekit-lsp"}
+}
 
 lsp.setup()
 
