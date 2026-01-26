e44cc1bd46091ee141cc29cdc334cb941874f672
Luqman Azhar <ContactMe@LuqmanAzhar.com>
2026-01-26 13:00:22 +0800

    Sway + nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -113,7 +113,7 @@ local plugins = {
     -----------------------------------------------------------------------------
     {
         'nvim-telescope/telescope.nvim',
-        branch = '0.1.x',
+        version = '*',
         dependencies = {
             'nvim-lua/plenary.nvim',
             -- Fuzzy Finder Algorithm which requires local dependencies to be built.
